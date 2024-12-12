f8a57d043834cf3c3851143e7aff429b0b485090
MRlolface249 <ahop089@aucklanduni.ac.nz>
2024-12-12 21:19:54 +1300

    added database setup, still needs connection string

--- Digraphy/Digraphy/Program.cs
+++ Digraphy/Digraphy/Program.cs
@@ -3,9 +3,8 @@ using Digraphy.Interfaces;
 using Digraphy.Mapper;
 using Digraphy.Repository;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -14,25 +13,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(typeof(MappingProfiles));
 builder.Services.AddScoped<ITodoRepository, TodoRepository>();
-builder.Services.AddDbContext<AppDbContext>(opt =>
-    opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDbContextPool<AppDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DigraphyDatabase")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.WebHost.UseUrls("http://localhost:5297");
-builder.WebHost.ConfigureKestrel((context, opts) =>
-{
-    opts.Limits.MinRequestBodyDataRate = new MinDataRate(2, TimeSpan.FromSeconds(10));
-});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        // Update the SwaggerUI configuration to specify the base URL
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "DigraphyApi");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
